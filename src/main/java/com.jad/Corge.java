4412badb80b1a1ad18973fc9f6865b3d526e22ad
florianthubin <thubin.florian@gmail.com>
2025-06-10 10:38:11 +0200

    Corge done

--- src/main/java/com.jad/Corge.java
+++ src/main/java/com.jad/Corge.java
@@ -1,4 +1,23 @@
 package com.jad;
 
 public class Corge {
+    private Foo foo;
+
+    public Foo getFoo() {
+        return this.foo;
+    }
+
+    public void setFoo(Foo foo) {
+        this.foo = foo;
+        if (this.foo.getCorge() != this) {
+            this.foo.setCorge(this);
+        }
+    }
+
+    public Corge(Foo foo) {
+        this.setFoo(foo);
+    }
+
+
+
 }
--- src/main/java/com.jad/Foo.java
+++ src/main/java/com.jad/Foo.java
@@ -1,8 +1,31 @@
 package com.jad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Foo {
 
     private final Bar bar;
+    private final ArrayList <Baz> bazs= new ArrayList<>();
+    private final Qux qux = new Qux();
+    private Corge corge;
+
+
+    public Corge getCorge() {
+        return this.corge;
+    }
+
+    public void setCorge(Corge corge) {
+        if (this.corge != null) {
+            this.corge.setFoo(null);
+        }
+        this.corge = corge;
+        if (this.corge.getFoo() != this) {
+            this.corge.setFoo(this);
+        }
+    }
+
+
 
     public Foo(Bar bar) {
         this.bar = bar;
@@ -11,4 +34,13 @@ public class Foo {
     public Bar getBar() {
         return this.bar;
     }
+    public List<Baz> getBazs() {
+        return new ArrayList<>(this.bazs);
+    }
+
+    public void addBaz(final Baz baz) {
+        this.bazs.add(baz);
+    }
+
+
 }
