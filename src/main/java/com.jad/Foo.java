9f667564dc6496fae1b907e415e054aa9baab6f2
florianthubin <thubin.florian@gmail.com>
2025-06-10 10:11:37 +0200

    bar done

--- src/main/java/com.jad/Foo.java
+++ src/main/java/com.jad/Foo.java
@@ -1,4 +1,14 @@
 package com.jad;
 
 public class Foo {
+
+    private final Bar bar;
+
+    public Foo(Bar bar) {
+        this.bar = bar;
+    }
+
+    public Bar getBar() {
+        return this.bar;
+    }
 }
